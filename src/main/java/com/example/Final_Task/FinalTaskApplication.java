f268797f6a5c862dedb8af352fe34d518bba4517
vrusakov <rusakov_vas@mail.ru>
2024-06-27 10:04:55 +0500

    Дополнены зависимости, перенесена SpringBootApplication

--- src/main/java/com/example/Final_Task/FinalTaskApplication.java
+++ src/main/java/com/example/Final_Task/FinalTaskApplication.java
@@ -1,10 +1,12 @@
 package com.example.Final_Task;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FinalTaskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FractionSimplifier.class, args);
+		SpringApplication.run(FinalTaskApplication.class, args);
 	}
 }
